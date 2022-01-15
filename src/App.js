60199edce6bed753e344e74794a6f5f3c5d749db
Sambuka <vova.kurtiak@gmail.com>
2022-01-15 12:23:15 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useGetPostsQuery } from "./redux/postsApi";
+
 
 function App() {
+  const {data =[], isLoading} = useGetPostsQuery();
+
+  if(isLoading) {
+    <h1>Loading...</h1>
+  }
+
+  console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {data.map((item) => (<li key = {item.id}>{item.title}</li>))}
+      </ul>
     </div>
   );
 }
