70c91ae06ffa63e7e5b531b41bffef505761a5aa
Sambuka <vova.kurtiak@gmail.com>
2022-01-18 18:31:28 +0100

    crud done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import { useGetPostsQuery } from "./redux/postsApi";
+import { useState } from 'react'
+import { useGetPostsQuery, useAddPostMutation, useDeletePostMutation } from "./redux/postsApi";
 
 
 function App() {
-  const {data =[], isLoading} = useGetPostsQuery();
+  const [count, setCount] = useState('');
+  const [newPost, setNewPost] = useState('');
+  const {data =[], isLoading} = useGetPostsQuery(count);
+  const [addPost, {isError}] = useAddPostMutation();
+  const [deleteProduct] = useDeletePostMutation();
+
+  const handleAddPost = async () => {
+    if(newPost) {
+      await addPost({title: newPost}).unwrap();
+      setNewPost('');
+    }
+  }
+
+  const handleDeleteProduct = async (id) => {
+    await deleteProduct(id).unwrap();
+  }
 
   if(isLoading) {
     <h1>Loading...</h1>
@@ -12,8 +28,22 @@ function App() {
 
   return (
     <div className="App">
+    <div>
+      <input 
+      type="text" 
+      value={newPost}
+      onChange={(e) => setNewPost(e.target.value)}
+      />
+      <button onClick={handleAddPost}>Add</button>
+    </div>
+    <select value={count} onChange={(e) => setCount(e.target.value)}>
+      <option value="">All</option>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
       <ul>
-        {data.map((item) => (<li key = {item.id}>{item.title}</li>))}
+        {data.map((item) => (<li key = {item.id} onClick={() => handleDeleteProduct(item.id)}>{item.title}</li>))}
       </ul>
     </div>
   );
--- src/redux/postsApi.js
+++ src/redux/postsApi.js
@@ -2,12 +2,35 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const postsApi = createApi({
     reducerPath: 'postsApi',
+    tagTypes: ['Posts'],
     baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3001/'}),
     endpoints: (builder) => ({
         getPosts: builder.query({
-            query: () => 'posts',
+            query: (limit='') => `posts?${limit && `_limit=${limit}`}`,
+            providesTags: (result) =>
+            result
+              ? [
+                  ...result.map(({ id }) => ({ type: 'Posts', id })),
+                  { type: 'Posts', id: 'LIST' },
+                ]
+              : [{ type: 'Posts', id: 'LIST' }],
+        }),
+        addPost: builder.mutation({
+            query: (body) => ({
+                url: 'posts',
+                method: 'POST',
+                body
+            }),
+            invalidatesTags: [{type: "Posts", id: "LIST"}]
+        }),
+        deletePost: builder.mutation({
+            query: (id) => ({
+                url: `posts/${id}`,
+                method: 'DELETE'
+            }),
+            invalidatesTags: [{type: "Posts", id: "LIST"}]
         })
     })
 })
 
-export const {useGetPostsQuery} = postsApi;
+export const {useGetPostsQuery, useAddPostMutation, useDeletePostMutation} = postsApi;
